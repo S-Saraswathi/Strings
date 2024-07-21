f81e23ccd2fc184460c4fc2c1f27c1bd510e7c5d
Saraswathi S <139524048+S-Saraswathi@users.noreply.github.com>
2024-07-21 23:26:04 +0530

    Update name_disection.cpp

--- name_disection.cpp
+++ name_disection.cpp
@@ -1,14 +1,17 @@
+#PRN:22070123102
+#Exp-8
+
 #include<iostream>
 #include<string.h>
 using namespace std;
 int main(){
     char str[100] = {'h','e','l','l','o'};
-    cout<<str<<endl<<"Enter your name and surname:";
+    cout<<str<<endl<<"Enter your name and surname: ";
     char name[100];
     cin.get(name ,100);
     cout<<name<<endl;
-    int length=0,length1=0;
-    for(int i=0;name[i]!='\0';i++)
+    int length=0,length1=0,length2=0;
+    for(int i=0;name[i]!=' ';i++)
     {
         length++;
     }
@@ -21,7 +24,8 @@ int main(){
         length1++;
         ptr++;
     }
-    cout<<length1<<endl;
+    length2=(length1-length)-1;
+    cout<<length2<<endl;
 
     int onlyname=0,surname;
     ptr=&name[0];
